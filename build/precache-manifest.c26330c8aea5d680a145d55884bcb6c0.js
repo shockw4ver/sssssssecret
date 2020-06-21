9f0e8b14dc484c4908eda3d5a27a11bebaaf71d9
Samuel <asambojur@live.cn>
2020-06-21 23:52:51 +0800

    add homepage

---
+++ build/precache-manifest.c26330c8aea5d680a145d55884bcb6c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3798f1107200e338ddc99404f609628f",
+    "url": "/sir/index.html"
+  },
+  {
+    "revision": "a695ef298c5e8a829473",
+    "url": "/sir/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "3ad3fe50ee8dcfda6576",
+    "url": "/sir/static/js/2.77833a2c.chunk.js"
+  },
+  {
+    "revision": "a695ef298c5e8a829473",
+    "url": "/sir/static/js/main.aa11e9af.chunk.js"
+  },
+  {
+    "revision": "9d59a3432a6ecfe131fa",
+    "url": "/sir/static/js/runtime~main.0a3b95f7.js"
+  }
+]);
